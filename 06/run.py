8b643fe4ec95efe8c84a2f1a79e1b93c938ed4b1
Robert Franke <robert.franke@nirx.de>
2024-12-10 21:27:55 +0100

    Day 06, part 2

--- 06/run.py
+++ 06/run.py
@@ -11,26 +11,44 @@ def turn_right(direction):
         case (0, -1): return (-1, 0)
         case (-1, 0): return (0, 1)
 
+
+def walk(field, start_pos):
+    nrows, ncols = len(field), len(field[0])
+    pos = start_pos
+    direction = (-1, 0)
+    visited = {(pos, direction)}
+
+    while True:
+        new_pos = (pos[0] + direction[0], pos[1] + direction[1])
+        if new_pos[0] < 0 or new_pos[0] >= nrows or new_pos[1] < 0 or new_pos[1] >= ncols:
+            break
+        elif field[new_pos[0]][new_pos[1]] == "#":
+            direction = turn_right(direction)
+        else:
+            pos = new_pos
+            if field[pos[0]][pos[1]] == ".":
+                if (pos, direction) in visited:
+                    return True, visited
+                visited.add((pos, direction))
+
+    return False, visited
+
 input = sys.argv[1] if len(sys.argv) > 1 else "input"
 field = [list(line.rstrip("\n")) for line in open(input)]
-nrows, ncols = len(field), len(field[0])
-pos = [(i_line, line.index("^")) for i_line, line in enumerate(field) if "^" in line][0]
-print(f"Start position: {pos}")
-direction = (-1, 0)
-visited = 1
+start_pos = [(i_line, line.index("^")) for i_line, line in enumerate(field) if "^" in line][0]
+print(f"Start position: {start_pos}")
+_, visited = walk(field, start_pos)
+distinct_pos = {pos for pos, _ in visited}
+print(f"Visited {len(distinct_pos)} cells")
 
-while True:
-    new_pos = (pos[0] + direction[0], pos[1] + direction[1])
-    if new_pos[0] < 0 or new_pos[0] >= nrows or new_pos[1] < 0 or new_pos[1] >= ncols:
-        break
-    elif field[new_pos[0]][new_pos[1]] == "#":
-        direction = turn_right(direction)
-    else:
-        pos = new_pos
-        if field[pos[0]][pos[1]] == ".":
-            visited += 1
-        field[pos[0]][pos[1]] = "X"
-        # print_field(field)
-        # print()
+# Part 2
+loops = 0
+for cell in distinct_pos - {start_pos}:
+    field[cell[0]][cell[1]] = "#"
+    loop, _ = walk(field, start_pos)
+    if loop:
+        loops += 1
+        # print(cell, loop)
+    field[cell[0]][cell[1]] = "."
 
-print(visited)
+print(f"Found {loops} loops")
